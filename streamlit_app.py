354475739d153839dca56798bda24b8bc4260419
peacelogeswaran007 <logeswaran641607@gmail.com>
2025-05-18 00:28:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import joblib
 
 # Load the trained model
-model = joblib.load('train_model.joblib')
+model = joblib.load('best_model.joblib')
 
 # Streamlit app UI
 st.title("🏠 House Price Prediction App")
